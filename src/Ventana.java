a99a06ec28554dd254cf6807c7e47ac05701cdbf
DanielRojasPorras <daniel.rojas.porras@est.una.ac.cr>
2020-09-04 18:10:09 -0500

    Primer commit java

---
+++ src/Ventana.java
@@ -0,0 +1,22 @@
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout; Bordes
+import java.awt.GridLayout; //Tabla
+import java.awt.FlowLayout;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author (2020) Daniel Rojas Porras
+ */
+public class Ventana extends JFrame {
+    public Ventana(){
+        
+    }
+    
+}
