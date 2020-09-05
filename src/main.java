c494211d6cbd550e443675b97f9b305ac89181ad
DanielRojasPorras <daniel.rojas.porras@est.una.ac.cr>
2020-09-04 21:41:25 -0500

    main

---
+++ src/main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author (2020) Daniel Rojas Porras
+ */
+public class main {
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+        v.setBounds(200,200,400,260);
+    }
+}
